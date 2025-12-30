6cab2013940417a60c95a4d0244dc0c60946dea4
Justine <152494594+Justine-tblt@users.noreply.github.com>
2025-12-30 21:49:47 +0100

    error

--- js/workout.js
+++ js/workout.js
@@ -1,3 +1,18 @@
+// templates des séances
+export const templates = [
+  {
+    id: "A",
+    name: "Séance A",
+    exercises: ["Bench Press", "OHP", "Dips"]
+  },
+  {
+    id: "B",
+    name: "Séance B",
+    exercises: ["Rowing", "Tractions", "Curl"]
+  }
+]
+
+// fonction pour démarrer une séance
 export function startWorkout(template) {
   return {
     templateId: template.id,
